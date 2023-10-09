8ec27033b76fbf2c52e42a88423c58b3b7254d58
RMP <ruthiepina@gmail.com>
2023-10-08 21:41:51 -0600

    Created array & used for loop to perform task

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
-var topic = "HTML";
-
-if (topic === 'HTML') {
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
+/*if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
   console.log("Let's study CSS!");
@@ -11,3 +13,4 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
+*/
